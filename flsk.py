61f2f8461949820cc68b7e74ea4d87b65dac9e69
DiegoGca <diegogcaalcala@usal.es>
2019-09-15 06:05:41 +0000

    Added Flask

--- flsk.py
+++ flsk.py
@@ -1,4 +1,4 @@
-
+import re
 from flask import Flask
 from flask import request
 
@@ -6,19 +6,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello world<h1>'
+    return '<h1>hello world<h1>'  # TODO: add default page
 
 @app.route('/test/', strict_slashes=False)
 def test():
     return '<h1>test<h1>'
 
 
-@app.route('/postjson', methods = ['POST'])
-def postJsonHandler():
-    print (request.is_json)
+@app.route('/postjson/<id>/', methods = ['POST'], strict_slashes=False)
+def postJsonHandler(id):
+    print(request.is_json)
     content = request.get_json()
-    print (content)
-    return 'JSON posted'
+    for t in content['tunnels']:
+        if t['name'] == id:
+            #print(t['name'])
+            #print(t['public_url'])
+            port = re.search(':[0-9]+',t['public_url']).group(0)
+            port = re.search('[0-9]+', port).group(0)
+            #print(port)
+    return port
 
 
 
